657374d7a5a73982e86406984cc4ee5c4140e680
Bagas Hilmi <bagashilmiarib99@gmail.com>
2024-08-07 16:21:39 +0700

    CRUD Update Data di Database

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,8 +22,12 @@ Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
 
-
-
+//route CRUD
+Route::get('/pegawai', [PegawaiController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiController::class, 'store']);
+Route::get('/pegawai/edit/{id}',[PegawaiController::class, 'edit']);
+Route::post('/pegawai/update',[PegawaiController::class, 'update']);
 
 
 
