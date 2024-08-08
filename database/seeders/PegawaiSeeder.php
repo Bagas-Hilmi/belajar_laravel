0deef18e7032762ea739c8f81ed4ee4ee35a58db
Bagas Hilmi <bagashilmiarib99@gmail.com>
2024-08-08 09:11:38 +0700

    Seeder Dan faker input data dummy

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -1,7 +1,10 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB; // Tambahkan ini
+use Faker\Factory as Faker;
 
 class PegawaiSeeder extends Seeder
 {
@@ -12,12 +15,17 @@ class PegawaiSeeder extends Seeder
      */
     public function run()
     {
-        // insert data ke table pegawai
-        DB::table('pegawai')->insert([
-            'pegawai_nama' => 'Joni',
-            'pegawai_jabatan' => 'Web Designer',
-            'pegawai_umur' => 25,
-            'pegawai_alamat' => 'Jl. Panglateh'
-        ]);
+        $faker = Faker::create('id_ID');
+ 
+    	for($i = 1; $i <= 50; $i++){
+ 
+    	      // insert data ke table pegawai menggunakan Faker
+    		DB::table('pegawai')->insert([
+    			'pegawai_nama' => $faker->name,
+    			'pegawai_jabatan' => $faker->jobTitle,
+    			'pegawai_umur' => $faker->numberBetween(25,40),
+    			'pegawai_alamat' => $faker->address
+    		]);
+        }
     }
 }
